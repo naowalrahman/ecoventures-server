a54ff9b9a3dd19e9d16da236f7b8823ab41e8776
Naowal Rahman <naowal.ar@gmail.com>
2023-03-18 21:43:47 -0400

    backend api call so far

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
-const port = 3001;
+const app = express()
+const port = 3001
 
-app.use(cors());
+app.use(cors())
 
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/details', (req, res) => {
-    date = new Date();
-    res.send({data: date.getFullYear() + "-" +
-              date.getMonth() + "-" + 
-              date.getDate() + ", " +
-              date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()});
-});
+const API_KEY = 'ab2695956bc5adc46b7e6f54f47a9d33'
+const locations = { NY: [40.7128, 74.006] }
+
+function getLocationData(lon, lat) {
+    let locationData
+    fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lon=${lon}&lat=${lat}&appid=${API_KEY}`)
+        .then((response) => response.json())
+        .then((json) => (locationData = json))
+
+    return locationData
+}
+
+function setupLocationAddresses() {
+    app.get('/location/(:lon)(-(:lat))?', (req, res) => {
+        let params = req.params
+        let dt = new Date()
+        dt =
+            dt.getFullYear() +
+            '-' +
+            dt.getMonth() +
+            '-' +
+            dt.getDate() +
+            ', ' +
+            dt.getHours() +
+            ':' +
+            dt.getMinutes() +
+            ':' +
+            dt.getSeconds()
+
+        let locationData
+        if (params['lat'] == undefined) {
+            locationData = getLocationData(locations[params['lon']][0], locations[params['lon']][1])
+        } else {
+            locationData = getLocationData((params['lon'], params['lat']))
+        }
+
+        console.log(locationData)
+
+        res.send({
+            date: dt,
+            aqi: locationData.list[0].main.aqi,
+            gas: locationData.list[0].components,
+        })
+    })
+}
+
+setupLocationAddresses()
+// app.get('/details', (req, res) => {
+//     date = new Date()
+//     res.send({
+//         data:
+//             date.getFullYear() +
+//             '-' +
+//             date.getMonth() +
+//             '-' +
+//             date.getDate() +
+//             ', ' +
+//             date.getHours() +
+//             ':' +
+//             date.getMinutes() +
+//             ':' +
+//             date.getSeconds(),
+//     })
+// })
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-
-module.exports = app;
