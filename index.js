036e6d1eadd449e25749ea6d96dabd0f7fb16743
Tawseef-Reza <65203327+Tawseef-Reza@users.noreply.github.com>
2023-05-25 20:36:42 -0400

    Finished Distance Calculator API

--- index.js
+++ index.js
@@ -40,8 +40,37 @@ function avgGas(gases) {
 
     return numerator / denominator;
 }
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return Math.round(d);
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
 
-function setupLocationAddresses() {
+  function returnRGB(distance) {
+    const equatedValue = Math.min(distance/35, 510) 
+    const darkFactor = 0.9;
+    const otherValue = 255 * darkFactor;
+    const blueVal = 96 * darkFactor;
+    if (equatedValue<=255) {
+        return `rgba(${equatedValue * 0.5}, ${otherValue}, ${blueVal}, 1)`
+    }
+    else {
+        return `rgba(${otherValue}, ${(510-equatedValue) * 0.5}, ${blueVal}, 1)`
+    }
+}
+function setupLocationAddressesAndDistances() {
     app.get('/location/:lat/:lon', (req, res) => {
         let params = req.params
         let dt = dateStr()
@@ -64,8 +93,36 @@ function setupLocationAddresses() {
         let params = req.params
         res.redirect(`/location/${locations[params.city].lat}/${locations[params.city].lng}`)
     })
+    // distance + color stuff
+    app.get('/location/:homeCity-:otherCity', (req, res) => {
+        let params = req.params
+        lat1 = locations[params.homeCity].lat
+        lon1 = locations[params.homeCity].lng
+        lat2 = locations[params.otherCity].lat
+        lon2 = locations[params.otherCity].lng
+        
+        let distanceMeasured = getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2)
+        res.send({
+            distance: distanceMeasured,
+            rgb: returnRGB(distanceMeasured)
+        })
+    })
+
+    app.get('/allDistance/:cityName', (req, res) => {
+        let params = req.params
+        let lat1 = locations[params.cityName]["lat"]
+        let lon1 = locations[params.cityName]["lng"]
+        distColorList = []
+        for (let property in locations) {
+            let distanceMeasured = getDistanceFromLatLonInKm(lat1, lon1, locations[property]["lat"], locations[property]["lng"])
+            distColorList.push([property, distanceMeasured, returnRGB(distanceMeasured)])
+        }
+        res.send({
+            dcList: distColorList
+        })
+    })
 
 }
 
-setupLocationAddresses()
+setupLocationAddressesAndDistances()
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
