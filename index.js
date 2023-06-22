e35f639fa7c8cbbd1969107edfb1cbc4ae9880b1
Tawseef-Reza <65203327+Tawseef-Reza@users.noreply.github.com>
2023-06-22 00:22:03 -0400

    space

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ function setupReviewAddress() {
     })()
   })
 }
-
+ 
 const formatDate = (dateString) => {
   const options = { year: "numeric", month: "long", day: "numeric" }
   return new Date(dateString).toLocaleDateString(undefined, options)
